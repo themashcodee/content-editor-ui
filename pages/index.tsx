486efe480542f98a064f4f424e9b08815723964e
Mash Codee <codeemash@gmail.com>
2022-03-24 23:55:04 +0530

    build(css) :nail_care:: setup tailwindcss

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,20 +1,23 @@
-import type { NextPage } from 'next'
-import Head from 'next/head'
+import type { NextPage } from "next"
+import Head from "next/head"
 
 const Home: NextPage = () => {
-  return (
-    <>
-      <Head>
-        <title>Content Editor</title>
-        <meta name="description" content="Content Editor - LRNR Interview assignement" />
-        <link rel="icon" href="/favicon.png" />
-      </Head>
+	return (
+		<>
+			<Head>
+				<title>Content Editor</title>
+				<meta
+					name="description"
+					content="Content Editor - LRNR Interview assignement"
+				/>
+				<link rel="icon" href="/favicon.png" />
+			</Head>
 
-      <main>
-        <h1>Content Editor</h1>
-      </main>
-    </>
-  )
+			<main className="p-12">
+				<h1 className="text-4xl font-semibold">Content Editor</h1>
+			</main>
+		</>
+	)
 }
 
 export default Home
