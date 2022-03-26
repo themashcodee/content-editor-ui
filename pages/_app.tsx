ed8570b057e510485b122d395b680cdbdb316286
Mash Codee <codeemash@gmail.com>
2022-03-26 13:19:53 +0530

    feat(toast): add react-hot-toast

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,11 +1,13 @@
 import "../styles/globals.css"
 import type { AppProps } from "next/app"
 import { RecoilRoot } from "recoil"
+import { Toaster } from "react-hot-toast"
 
 function MyApp({ Component, pageProps }: AppProps) {
 	return (
 		<RecoilRoot>
 			<Component {...pageProps} />
+			<Toaster />
 		</RecoilRoot>
 	)
 }
