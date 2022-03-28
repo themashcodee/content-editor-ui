6fc08d806c431c0d4194be091909dad1c15618bd
Mash Codee <codeemash@gmail.com>
2022-03-29 01:19:02 +0530

    fix :ambulance:: fix editor visibility issue when changing tab

--- components/pages/home/subheader/tab/subheader-tab.tsx
+++ components/pages/home/subheader/tab/subheader-tab.tsx
@@ -7,6 +7,8 @@ import React, {
 import { Capitalize } from "helpers"
 
 import { Tabs } from "../main"
+import { useSetRecoilState } from "recoil"
+import { SelectedFileAtom } from "store"
 
 type P = {
 	selected: boolean
@@ -16,9 +18,14 @@ type P = {
 
 export const Tab: NamedExoticComponent<P> = memo(
 	({ selected, setSelectedTab, value }) => {
+		const setSelectedFile = useSetRecoilState(SelectedFileAtom)
+
 		return (
 			<button
-				onClick={() => setSelectedTab(value)}
+				onClick={() => {
+					setSelectedFile(null)
+					setSelectedTab(value)
+				}}
 				className={`cursor-pointer h-full flex items-center px-5 ${
 					selected ? "border-b-2 border-red-400" : ""
 				}`}
