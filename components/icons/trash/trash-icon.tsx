4ab6358d16fe0fe0640c36a689130fcd6f1d6ff7
Mash Codee <codeemash@gmail.com>
2022-03-28 12:55:43 +0530

    feat(icons) :sparkles::add trash icon custom reusable icon

---
+++ components/icons/trash/trash-icon.tsx
@@ -0,0 +1,24 @@
+import React, { ComponentPropsWithoutRef } from "react"
+import { Component } from "types/common"
+
+type P = ComponentPropsWithoutRef<"svg">
+
+export const TrashIcon: Component<P> = ({ className, ...props }) => {
+	return (
+		<svg
+			xmlns="http://www.w3.org/2000/svg"
+			className="h-4 w-4"
+			fill="none"
+			viewBox="0 0 24 24"
+			stroke="currentColor"
+			strokeWidth={2}
+			{...props}
+		>
+			<path
+				strokeLinecap="round"
+				strokeLinejoin="round"
+				d="M19 7l-.867 12.142A2 2 0 0116.138 21H7.862a2 2 0 01-1.995-1.858L5 7m5 4v6m4-6v6m1-10V4a1 1 0 00-1-1h-4a1 1 0 00-1 1v3M4 7h16"
+			/>
+		</svg>
+	)
+}
